d72a0021dc5d922685460cdc7941ac40c8f85c42
Chubbard022 <chubbard022@gmail.com>
2019-04-30 13:15:12 -0700

    delete functionality working

--- zoo/zoo-router.js
+++ zoo/zoo-router.js
@@ -75,4 +75,19 @@ router.put('/:id', (req, res) => {
       });
   });
 
+router.delete("/:id",(req,res)=>{
+    db("zoos")
+    .where({id: req.params.id})
+    .del()
+    .then(count =>{
+        if(count > 0) {
+            res.status(200).json({message: `${count} ${count > 1 ? 'records' : 'record'} deleted`})
+        }else {
+            res.status(404).json({ message: 'Role does not exist' });
+          }
+    })
+    .catch(err => {
+        res.status(500).json(err);
+      });
+})
 module.exports = router;
